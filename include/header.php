46d49d477e7f926686a577a79648cd4a40bedb7a
Matthieu Ramanitrera <mrama100@uottawa.ca>
2024-04-08 18:59:02 -0400

    Create landing page

--- include/header.php
+++ include/header.php
@@ -2,11 +2,25 @@
 <html>
     <head>
         <title></title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
         
         <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
     </head>
+
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <h5 class="text-white">Hospital Triage</h5>
+            
+            <div class="mr-auto"></div>
+
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link text-white">Admin</a></li>
+                <li class="nav-item"><a href="" class="nav-link text-white">Patient</a></li>
+            </ul>
+
+        </nav>
+    </body>
 </html>
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hospital Triage</title>
+    </head>
+    <style>
+        .hero-section {
+            background-color: #f8f9fa;
+            padding: 100px 0;
+        }
+        .hero-content {
+            margin-top: 30px;
+        }
+  </style>
+    <body>
+        <?php
+            include("include/header.php");
+        ?>
+
+    <section class="hero-section">
+        <div class="container">
+        <div class="row align-items-center">
+            
+            <div class="col-md-6">
+            <img src="image.png" class="img-fluid" alt="Image">
+            </div>
+            
+            <div class="col-md-6 hero-content">
+            <h2>Welcome to Hospital Triage</h2>
+            <p class="lead">Hospital Triage helps admins and patients better understand waiting times in the emergency room.</p>
+            <form>
+                <div class="form-group">
+                <input type="text" class="form-control" placeholder="Username">
+                </div>
+                <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password">
+                </div>
+                <button type="submit" class="btn btn-primary btn-block">Login</button>
+            </form>
+            </div>
+        </div>
+        </div>
+    </section>
+    </body>
+</html>
